2786cf98e6af25a3818ed3bcaeaa19700352b808
Sipeng Xie <SipengXie@buaa.edu.cn>
2021-05-26 19:05:20 +0800

    毕业设计chaincode

--- record.go
+++ record.go
@@ -13,26 +13,27 @@ type SimpleChaincode struct {
 }
 
 type dataRecord struct {
-	DocType string `json:"docType""`
-	Id string `json:"id"`
-	Department string `json:"department"`
-	User string `json:"user"`
-	Object string `json:"object"`
-	Type string `json:"type"`
-	OpinionTime string `json:"opinionTime"`
-	Reviewer string `json:"reviewer"`
-	ReviewTime string `json:"reviewTime"`
-	ReviewResult string `json:"reviewResult"`
-	OperateTime string `json:"operateTime"`
-	Content string `json:"content"`
+	DocType          string `json:"docType"`
+	Id               string `json:"id"`
+	Department       string `json:"department"`
+	User             string `json:"user"`
+	Object           string `json:"object"`
+	Type             string `json:"type"`
+	OpinionTime      string `json:"opinionTime"`
+	Reviewer         string `json:"reviewer"`
+	ReviewTime       string `json:"reviewTime"`
+	ReviewResult     string `json:"reviewResult"`
+	ReviewDepartment string `json:"reviewDepartment"`
+	OperateTime      string `json:"operateTime"`
+	Content          string `json:"content"`
 }
 
 type userRecord struct {
-	DocType string `json:"docType"`
-	Department string `json:"department"`
-	UserName string `json:"userName"`
+	DocType     string `json:"docType"`
+	Department  string `json:"department"`
+	UserName    string `json:"userName"`
 	UserAddress string `json:"userAddress"`
-	Role string `json:"role"`
+	Role        string `json:"role"`
 }
 
 func (t *dataRecord) UnReviewed() bool {
@@ -54,40 +55,50 @@ func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface) pb.Response {
 	return shim.Success(nil)
 }
 
-func (t *SimpleChaincode) Invoke (stub shim.ChaincodeStubInterface) pb.Response {
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
 	function, args := stub.GetFunctionAndParameters()
 	fmt.Println("Invoking is running" + function)
 	switch function {
-	case "initDataRecord"		   : return t.initDataRecord(stub, args)
-	case "initUserRecord" 		   : return t.initUserRecord(stub, args)
-	case "modifyDataRecord" 	   : return t.modifyDataRecord(stub, args)
-	case "modifyUserRecord" 	   : return t.modifyUserRecord(stub, args)
-	case "queryDataRecordByObject" : return t.queryDataRecordByObject(stub, args)
-	case "queryDataRecordById"     : return t.queryDataRecordById(stub, args)
-	case "queryDataRecordByUser"   : return t.queryDataRecordByUser(stub, args)
-	case "queryUserRecordByDept"   : return t.queryUserRecordByDept(stub, args)
-	case "queryUserRecordByAddr"   : return t.queryUserRecordByAddr(stub, args)
-	case "queryWithQueryString"    : return t.queryWithQueryString(stub, args)
-	default:return shim.Error("Received unknown function invocation")
+	case "initDataRecord":
+		return t.initDataRecord(stub, args)
+	case "initUserRecord":
+		return t.initUserRecord(stub, args)
+	case "modifyDataRecord":
+		return t.modifyDataRecord(stub, args)
+	case "modifyUserRecord":
+		return t.modifyUserRecord(stub, args)
+	case "queryDataRecordByObject":
+		return t.queryDataRecordByObject(stub, args)
+	case "queryDataRecordById":
+		return t.queryDataRecordById(stub, args)
+	case "queryDataRecordByUser":
+		return t.queryDataRecordByUser(stub, args)
+	case "queryUserRecordByDept":
+		return t.queryUserRecordByDept(stub, args)
+	case "queryUserRecordByAddr":
+		return t.queryUserRecordByAddr(stub, args)
+	case "queryWithQueryString":
+		return t.queryWithQueryString(stub, args)
+	default:
+		return shim.Error("Received unknown function invocation")
 	}
 }
 
 func (t *SimpleChaincode) initDataRecord(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	//  0    1           2    3      4   5               6         7          8            9           10
-	//  uuid Department1 Jack object add opinionTime     reviewer  reviewTime reviewResult operateTime [contentHash]
+	// 0    1           2    3      4   5
+	// uuid Department1 Jack object add opinionTime
+	// 0    1           2    3      4   5           6
+	// uuid Department1 Jack object add operateTime [contentHash]
 	var err error
-	if len(args) != 11 {
-		return shim.Error("Incorrect number of arguments, expecting 11")
+	if len(args) != 6 && len(args) != 7 {
+		return shim.Error("Incorrect number of arguments, expecting 6 for records with opinion; expecting 7 for records without opinion")
 	}
 	// basic requirements of a data record
-	for i:= 0; i < 5 ; i += 1 {
+	for i := 0; i < len(args); i += 1 {
 		if len(args[i]) == 0 {
 			errMessage := fmt.Sprintf("Number %d member of arguments must be a non-empty string", i)
 			return shim.Error(errMessage)
 		}
-	}
-	if len(args[5]) == 0 && len(args[9]) == 0 {
-		return shim.Error("Invalid data record, must have opinion time or operate time")
 	}
 
 	docType := "dataRecord"
@@ -96,12 +107,17 @@ func (t *SimpleChaincode) initDataRecord(stub shim.ChaincodeStubInterface, args 
 	user := strings.ToLower(args[2])
 	object := strings.ToLower(args[3])
 	_type := strings.ToLower(args[4])
-	opinionTime := strings.ToLower(args[5])
-	reviewer := strings.ToLower(args[6])
-	reviewTime := strings.ToLower(args[7])
-	reviewResult := strings.ToLower(args[8])
-	operateTime := strings.ToLower(args[9])
-	content := strings.ToLower(args[10])
+	var opinionTime, operateTime, content string
+	opinionTime = ""
+	operateTime = ""
+	content = ""
+
+	if len(args) == 6 {
+		opinionTime = strings.ToLower(args[5])
+	} else {
+		operateTime = strings.ToLower(args[5])
+		content = strings.ToLower(args[6])
+	}
 
 	dataRecordJsonAsBytes, err := stub.GetState(uuid)
 	if err != nil {
@@ -118,9 +134,10 @@ func (t *SimpleChaincode) initDataRecord(stub shim.ChaincodeStubInterface, args 
 		object,
 		_type,
 		opinionTime,
-		reviewer,
-		reviewTime,
-		reviewResult,
+		"",
+		"",
+		"",
+		"",
 		operateTime,
 		content,
 	}
@@ -145,7 +162,7 @@ func (t *SimpleChaincode) initUserRecord(stub shim.ChaincodeStubInterface, args 
 	if len(args) != 4 {
 		return shim.Error("Incorrect number of arguments for user, expecting 5")
 	}
-	for i:= 0; i < 4 ; i += 1 {
+	for i := 0; i < 4; i += 1 {
 		if len(args[i]) <= 0 {
 			errMessage := fmt.Sprintf("No.%d argument must be a non-empty string", i)
 			return shim.Error(errMessage)
@@ -184,20 +201,17 @@ func (t *SimpleChaincode) initUserRecord(stub shim.ChaincodeStubInterface, args 
 }
 
 func (t *SimpleChaincode) modifyDataRecord(stub shim.ChaincodeStubInterface, args []string) pb.Response {
-	// 0    1      2        3		   4
-	// uuid review reviewer reviewTime reviewResult
-	// 0    1       2           3
-	// uuid operate operateTime [contentHash]
+	// 0     1        2		     3             4
+	// uuid  reviewer reviewTime reviewResult  reviewDepartment   for review
+	// 0     1           2
+	// uuid  operateTime [contentHash]   						  for operate
 	var err error
-	if len(args) != 5 && len(args) != 4 {
-		return shim.Error("Incorrect number of arguments: expecting 5 for review, 4 for operate")
+	if len(args) != 5 && len(args) != 3 {
+		return shim.Error("Incorrect number of arguments: expecting 5 for review, 3 for operate")
 	}
 	fmt.Println("Starting modifyDataRecord")
 	uuid := strings.ToLower(args[0])
-	op := strings.ToLower(args[1])
-	if op != "review" && op != "operate" {
-		return shim.Error("Unexpected operation code, expect \"review\" or \"operate\"")
-	}
+
 	dataRecordJsonAsBytes, err := stub.GetState(uuid)
 	if err != nil {
 		return shim.Error("Failed to get dataRecord: " + err.Error())
@@ -211,16 +225,18 @@ func (t *SimpleChaincode) modifyDataRecord(stub shim.ChaincodeStubInterface, arg
 		return shim.Error(err.Error())
 	}
 
-	if op == "review" {
+	if len(args) == 5 {
 		if !(dataRecordInstance.UnReviewed() && dataRecordInstance.UnOperated()) {
 			return shim.Error("The data record has been reviewed or operated, dataID: " + uuid)
 		}
-		reviewer := strings.ToLower(args[2])
-		reviewerTime := strings.ToLower(args[3])
-		reviewResult := strings.ToLower(args[4])
+		reviewer := strings.ToLower(args[1])
+		reviewerTime := strings.ToLower(args[2])
+		reviewResult := strings.ToLower(args[3])
+		reviewDepartment := strings.ToLower(args[4])
 		dataRecordInstance.Reviewer = reviewer
 		dataRecordInstance.ReviewTime = reviewerTime
 		dataRecordInstance.ReviewResult = reviewResult
+		dataRecordInstance.ReviewDepartment = reviewDepartment
 	} else {
 		if !dataRecordInstance.UnOperated() || dataRecordInstance.UnReviewed() {
 			return shim.Error("The data record has been operated or has not been reviewed, dataID: " + uuid)
@@ -228,12 +244,11 @@ func (t *SimpleChaincode) modifyDataRecord(stub shim.ChaincodeStubInterface, arg
 		if dataRecordInstance.ReviewResult == "false" {
 			return shim.Error("The opinion of this data record did not be approved, dataID: " + uuid)
 		}
-		operateTime := strings.ToLower(args[2])
-		content := strings.ToLower(args[3])
-		dataRecordInstance.OpinionTime = operateTime
+		operateTime := strings.ToLower(args[1])
+		content := strings.ToLower(args[2])
+		dataRecordInstance.OperateTime = operateTime
 		dataRecordInstance.Content = content
 	}
-
 	dataRecordJsonAsBytes, _ = json.Marshal(dataRecordInstance)
 	err = stub.PutState(uuid, dataRecordJsonAsBytes)
 	if err != nil {
@@ -242,11 +257,11 @@ func (t *SimpleChaincode) modifyDataRecord(stub shim.ChaincodeStubInterface, arg
 	return shim.Success(nil)
 }
 
-func (t *SimpleChaincode) modifyUserRecord(stub shim.ChaincodeStubInterface, args []string) pb.Response{
+func (t *SimpleChaincode) modifyUserRecord(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	// 0          1        2           3
 	// department username userAddress role
 	var err error
-	if len (args) != 4 {
+	if len(args) != 4 {
 		return shim.Error("Incorrect number of arguments, expecting 4")
 	}
 	department := strings.ToLower(args[0])
@@ -269,7 +284,7 @@ func (t *SimpleChaincode) modifyUserRecord(stub shim.ChaincodeStubInterface, arg
 	userRecordInstance.UserName = userName
 	userRecordInstance.Role = role
 
-	userRecordJsonAsBytes,err = json.Marshal(userRecordInstance)
+	userRecordJsonAsBytes, err = json.Marshal(userRecordInstance)
 	if err != nil {
 		return shim.Error(err.Error())
 	}
